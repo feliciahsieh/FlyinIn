e57d08d6969fbbfd5c4bf5ecb6875d2b6e988c15
feliciahsieh <felicia@tixwriteoff.com>
2018-02-07 20:59:12 +0000

    fixed scope problem with variable, data. It was outside of the parenthesis

--- testData.js
+++ testData.js
@@ -5,19 +5,21 @@ let driverStart = '94546';
 let driverEnd = 'KOAK';
 let url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' + driverStart + '&destinations=' + driverEnd + '&key=AIzaSyBoRvW47xXGNrYz-LYR3TLHC-p18sPFIes';
 
-//RUN JQUERY
+//RUN JQUERY AJAX
 $.get(url)
     .done(function (data) {
 	let driverDurationText = data.rows[0].elements[0].duration.text;
 	let driverDurationValue = data.rows[0].elements[0].duration.value; //in seconds
+
+
+	var flightArrive = ''; //in Epoch seconds
+	var timeCalc = '';
+
+	var timeToLeave = '5:05 PM';
+	var timeToArrive = '6:39 PM';
+	var resultText = 'Best time to Leave is <u><b>' + timeToLeave + ' +</b></u> (to arrive at ' + timeToArrive + ')';
+
+	$('#result').replaceWith(data.rows[0].elements[0].duration.text);
+
+
     });
-
-
-var flightArrive = ''; //in Epoch seconds
-var timeCalc = '';
-
-var timeToLeave = '5:05 PM';
-var timeToArrive = '6:39 PM';
-var resultText = 'Best time to Leave is <u><b>' + timeToLeave + ' +</b></u> (to arrive at ' + timeToArrive + ')';
-
-$('#result').replaceWith(data.rows[0].elements[0].duration.text);
