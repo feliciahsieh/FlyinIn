b835a8ad2857a7188725e8df30b3ece693d9110d
feliciahsieh <felicia@tixwriteoff.com>
2018-02-08 05:33:55 +0000

    use Phone DOM element in code

--- testTwilio.js
+++ testTwilio.js
@@ -4,9 +4,12 @@ var twilio = require('twilio', process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
 
 var time = '4:37 PM';
 var timeDiff = '1 hr(s) 15 min';
-var phoneNum = '+14156894626';
+var phoneNum;
 var phoneFI = '+14156894700';
 var msg = 'The best time to leave for the airport is at ' + time + ' in ' + timeDiff + '. Regards, Flyin In Team';
+
+phoneNum = '+14156894700'; //test phone number
+//phoneNum = $('#Phone').val();
 
 twilio.messages.create({
   to: phoneNum,
