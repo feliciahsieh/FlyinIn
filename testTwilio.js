5252d928469377117b2d4e3e4c4042cbddef85be
feliciahsieh <felicia@tixwriteoff.com>
2018-02-11 21:05:37 +0000

    prepare link to live Phone field

--- testTwilio.js
+++ testTwilio.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-var twilio = require('twilio', process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+let twilio = require('twilio', process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
-var time = '4:37 PM';
-var timeDiff = '1 hr(s) 15 min';
-var phoneNum;
-var phoneFI = '+14156894700';
-var msg = 'The best time to leave for the airport is at ' + time + ' in ' + timeDiff + '. Regards, Flyin In Team';
+let time = '4:37 PM';
+let timeDiff = '1 hr(s) 15 min';
+let phoneNum;
+let phoneFI = '+14156894700';
+let msg = 'The best time to leave for the airport is at ' + time + ' in ' + timeDiff + '. Regards, Flyin In Team';
 
 phoneNum = '+14156894700'; //test phone number
 //phoneNum = $('#Phone').val();
