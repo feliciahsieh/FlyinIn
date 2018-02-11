c0d145da5694993ce52d0b8de03cd2c75cc464bc
feliciahsieh <felicia@tixwriteoff.com>
2018-02-11 21:10:38 +0000

    remove 2 variables

--- testFALogin.js
+++ testFALogin.js
@@ -1,21 +1,24 @@
 //https://flightxml.flightaware.com/json/FlightXML3/FindFlight?origin=KPDX&destination=KOMA&type=nonstop
-var username = process.env.FLIGHTAWARE_USER;
-var key = process.env.FLIGHTAWARE_API3;
 
 var Client = require('node-rest-client').Client;
-var options_auth = { user: username, password: key };
+var options_auth = { user: process.env.FLIGHTAWARE_USER, password: process.env.FLIGHTAWARE_API3 };
 var client = new Client(options_auth);
 var fxmlUrl = 'https://flightxml.flightaware.com/json/FlightXML3/';
+
 client.registerMethod('findFlights', fxmlUrl + 'FindFlight', 'GET');
 var findFlightArgs = {
     parameters: {
         origin: 'SJC',
         destination: 'DEN',
-        type: 'nonstop'
+        type: 'nonstop',
+	howMany: 2
     }
 };
+
 client.methods.findFlights(findFlightArgs, function (data, response) {
     console.log(data);
+    console.log(data.FindFlightResult.flights[0]);
+    console.log(data.FindFlightResult.flights[1]);
     //console.log(response);
 });
 //client.on('error', function(err) {
