d1d9a779795b3e7c6864feed3a3c9eebf4f6b41e
feliciahsieh <felicia@tixwriteoff.com>
2018-02-12 21:32:01 +0000

    fix typo

--- testTwilio.js
+++ testTwilio.js
@@ -5,7 +5,7 @@ let twilio = require('twilio', process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
 let time = '5:37 PM';
 let phoneNum;
 let phoneFI = '+14156894700';
-let message = 'Best time to leave for the airport is at ' + time. Regards, Flyin In Team';
+let message = 'Best time to leave for the airport is at ' + time + '. Regards, Flyin In Team';
 
 phoneNum = '+14156894700'; //test phone number
 phoneNum = '+1' + $('#Phone').val();
