97f6066a6aa5d7c8166f75f840c441216a49aa97
feliciahsieh <felicia@tixwriteoff.com>
2018-02-13 03:43:44 +0000

    added a working curl query. Where is the FlightID?

--- testFlightStats.js
+++ testFlightStats.js
@@ -7,6 +7,7 @@ var key = '&appKey=Holberton\'s App.';
 var baseUri = 'https://api.flightstats.com/flex/flightstatus/rest/v2/json/' + pathInfo + query + appId + key;
 //https://api.flightstats.com/flex/flightstatus/v9/xml/fooservice?appId=12345678&appKey=abcdefghijklmnopqrstukwxyz123456v
 //curl -v  -X GET "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/SWA497?appId=ad8f1e96&appKey=Holberton%5C's+App."
+//curl -v  -X GET "https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/tracks/OAK/arr?appId=ad8f1e96&appKey=Holberton%5C's+App.&carrier=SWA&includeFlightPlan=false&maxPositions=2&maxFlights=5"
 
 //Check this out: https://github.com/danielpabbott/AirportAce/blob/master/public/app.js
 
